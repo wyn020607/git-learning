80d3ffb1ee6edcb4f76b8b216cea156093beb6c8
Wyn <2310225934@qq.com>
2026-05-25 15:18:12 +0800

    添加了两行注释

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
+//normal 递归
 long long factorial_recursive(int n) {
     if (n <= 1) return 1;
     return n * factorial_recursive(n - 1);
 }
-
+//tail 尾递归
 long long factorial_tail(int n, long long acc = 1) {
     if (n <= 1) return acc;
     return factorial_tail(n - 1, acc * n);
